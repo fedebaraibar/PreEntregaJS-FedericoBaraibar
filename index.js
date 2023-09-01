900dee8c8a372b63127bcb1071c8f70887ee1be8
fedebaraibar <fefebaraibar@gmail.com>
2023-08-31 23:18:51 -0300

    version 2 del promedio de alumnos

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
-function notas(notas1, nota2, nota3){
-    const sumaDeNotas = nota1 + nota2 + nota3
-    const notaFinal = sumaDeNotas / 3
-    if(notaFinal >= 7){
-        alert( notaFinal + ' TE FELICITO APROBASTE EL TRIMESTRE')
-    }
-    else{
-        alert( notaFinal + ' PONETE A ESTUDIAR YAAA')
+class Alumno {
+    constructor(nombre, apellido, nota1, nota2, nota3) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.nota1 = nota1
+        this.nota2 = nota2
+        this.nota3 = nota3
     }
 }
 
-alert('CONOCE TU PROMEDIO')
-const nota1 = parseFloat(prompt('INGRESA SU PRIMERA NOTA'))
-const nota2 = parseFloat(prompt('INGRESE SU SEGUNDA NOTA'))
-const nota3 = parseFloat(prompt('INGRESE SU TERCERA NOTA'))
+const alumnos = [];
 
-notas(nota1, nota2, nota3)
+const filtroAprobar = alumno => {
+    const promedio = (parseFloat(alumno.nota1) + parseFloat(alumno.nota2) + parseFloat(alumno.nota3)) / 3;
+    return promedio >= 7;
+};
+
+const filtroDesaprobar = alumno => {
+    const promedio = (parseFloat(alumno.nota1) + parseFloat(alumno.nota2) + parseFloat(alumno.nota3)) / 3;
+    return promedio < 7;
+};
+
+
+function sacarPromedio() {
+
+    const cantidadAlumnos = parseInt(prompt("CANTIDAD DE ALUMNOS:"))
+
+    for (let i = 0; i < cantidadAlumnos; i++) {
+
+        const nombre = prompt("NOMBRE DE ALUMNO:")
+
+        const apellido = prompt("APELLIDO DEL ALUMNO:")
+
+        const nota1 = parseFloat(prompt("PRIMERA NOTA DEL EXAMEN"))
+
+        const nota2 = parseFloat(prompt("NOTA DEL SEGUNDO EXAMEN"))
+
+        const nota3 = parseFloat(prompt(" NOTA DEL TERCER EXAMEN"))
+
+        const alumnosIngresados = new Alumno(nombre, apellido, nota1, nota2, nota3);
+
+        alumnos.push(alumnosIngresados);
+    }
+
+    const trimestreAprobado = alumnos.filter(filtroAprobar);
+    const trimestreDesaprobado = alumnos.filter(filtroDesaprobar);
+
+    console.log("todos los alumnos" + ' ' + alumnos.length)
+
+    console.log(alumnos)
+
+    console.log("CANTIDAD DE ALUMNOS QUE APROBARON EL TRIMESTRE" +  ''  + trimestreAprobado.length);
+
+
+    for (const trimestreAprobado of trimestreAprobado) {
+        console.log(trimestreAprobado.nombre, trimestreAprobado.apellido);
+    }
+
+    console.log("CANTIDAD DE ALUMNOS Q DESAPROBARON EL TRIMESTRE" +  ''  + trimestreDesaprobado.length);
+
+    for (const trimestreDesaprobado of trimestreDesaprobado) {
+        console.log(trimestreDesaprobado.nombre, trimestreDesaprobado.apellido);
+    }
+}
+sacarPromedio()
